1b55642563b3c3e61fa5df4f1beeb80ae6e6f164
Lindelor <leontev150@mail.ru>
2020-07-22 15:27:32 +0400

    Third task in part 2 complete

--- src/com/lind/epam/modul1/part/two/ThirdTask.java
+++ src/com/lind/epam/modul1/part/two/ThirdTask.java
@@ -1,5 +1,8 @@
 package com.lind.epam.modul1.part.two;
 
+//  Даны три точки А(х1,у1), В(х2,у2) и С(х3,у3).
+//  Определить, будут ли они расположены на одной прямой
+
 public class ThirdTask {
 
     public static void main(String[] args) {
