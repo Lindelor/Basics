d2bd84d2aa0facda9c7785bdce29012f01bade2f
Lindelor <leontev150@mail.ru>
2020-07-22 16:55:21 +0400

    5th task in part 2 is complete

---
+++ src/com/lind/epam/modul1/part/two/FifthTask.java
@@ -0,0 +1,30 @@
+package com.lind.epam.modul1.part.two;
+
+//    Вычислить значение функции
+//    x^2-3x+9, если x<=3
+//    1/(x^3 + 6), если x>3
+
+import static java.lang.Math.pow;
+
+public class FifthTask {
+
+    public static void main(String[] args) {
+
+        double x = 3;
+
+        func(x);
+    }
+
+    public static void func(double x) {
+        if (x <=3) {
+            System.out.println("Answer is: ");
+            System.out.println(pow(x, 2) - 3 * x + 9);
+        }
+        else {
+            System.out.println("Answer is: ");
+            System.out.println(1 / (pow(x, 3) + 6));
+        }
+
+    }
+
+}
