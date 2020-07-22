48a205e28b2ae1a66fb17290fd33210688f03401
Lindelor <leontev150@mail.ru>
2020-07-22 15:09:40 +0400

    Complete 2nd task in part 2

--- src/com/lind/epam/modul1/part/two/SecondTask.java
+++ src/com/lind/epam/modul1/part/two/SecondTask.java
@@ -1,4 +1,29 @@
 package com.lind.epam.modul1.part.two;
 
+// Найти max{min(a, b), min(c, d)}.
+//    в Java есть Math.max() и Math.min(), но тут вроде как ветвления проверяют, написал свой метод
+
+import java.util.Scanner;
+
 public class SecondTask {
+
+    public static void main(String[] args) {
+        float a = 1;
+        float b = 2;
+        float c = 3;
+        float d = 4;
+        System.out.println("Ответ: " + max(min(a, b), min(c, d)));
+
+    }
+
+    public static float max(final float a, final float b) {
+        if (a > b) return a;
+        return b;
+    }
+
+    public static float min(final float a, final float b) {
+        if (a > b) return b;
+        return a;
+    }
+
 }
