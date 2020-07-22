5ba26f2b646cd1368c7bf2d73f96b6e56b3d3192
Lindelor <leontev150@mail.ru>
2020-07-22 16:43:06 +0400

    4th task in part2 done

---
+++ src/com/lind/epam/modul1/part/two/FourthTask.java
@@ -0,0 +1,31 @@
+package com.lind.epam.modul1.part.two;
+
+//    Заданы размеры А, В прямоугольного отверстия и размеры х, у, z кирпича.
+//        Определить, пройдет ли кирпич через отверстие.
+
+public class FourthTask {
+
+    public static void main(String[] args) {
+
+        int sizeA = 2;
+        int sizeB = 3;
+        int x = 2;
+        int y = 4;
+        int z = 1;
+
+        System.out.println(new FourthTask().isSizeOk(sizeA, sizeB, x, y, z));
+
+    }
+
+    public boolean isSizeOk(int a, int b, int x, int y, int z) {
+
+        if (a >= x && (b >= y || b >= z)) {
+            return true;
+        }
+        else if (b >= x && (a >= y || a >= z)) {
+            return true;
+        }
+        return false;
+    }
+
+}
